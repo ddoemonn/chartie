8a3e3a3e2755112af37e08ec781cbd4ef5cfa3b3
ddoemonn <109994179+ddoemonn@users.noreply.github.com>
2025-06-24 00:32:58 +0300

    Delete lib/dist directory

--- lib/dist/charts/AreaChart.d.ts
+++
@@ -1,9 +0,0 @@
-import type { ChartConfig, ChartBounds } from '../types';
-import type { ChartInstance } from '../core/TinyChart';
-export declare class AreaChart implements ChartInstance {
-    private lineChart;
-    constructor(config: ChartConfig);
-    render(ctx: CanvasRenderingContext2D, bounds: ChartBounds, progress: number): void;
-    destroy(): void;
-}
-//# sourceMappingURL=AreaChart.d.ts.map
--- lib/dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { Chartie } from './core/TinyChart';
-export type { ChartType, ChartConfig, ChartOptions, Dataset, DataPoint, Color, AxisConfig, ChartBounds, AnimationFrame } from './types';
-export { getColorArray, hexToRgba, clamp, lerp, getMinMax, defaultColors, easingFunctions } from './utils/helpers';
-export declare const version = "0.1.0";
-//# sourceMappingURL=index.d.ts.map
